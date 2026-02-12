bcae976b7a486bf5a4fbef40554a7338fb0112dd
restrellado <rianestrellado3@gmail.com>
2026-02-12 14:34:50 +0800

    backend

---
+++ backend/src/main/java/org/example/capstone3/Model/Account.java
@@ -0,0 +1,34 @@
+package org.example.capstone3.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name ="accounts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_id")
+    private String accountId;
+
+    @Column(nullable = false )
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    private Double balance;
+
+
+}
---
+++ backend/src/main/java/org/example/capstone3/Repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package org.example.capstone3.Repository;
+
+import org.example.capstone3.Model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
